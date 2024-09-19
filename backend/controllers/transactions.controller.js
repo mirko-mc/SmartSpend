0068031e72c1f7c0a9edf71d3e48a274ff5403cf
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-09-19 10:51:05 +0000

    BE - transactions : definizione rotte router e funzioni vuote controller

--- backend/controllers/transactions.controller.js
+++ backend/controllers/transactions.controller.js
@@ -1,1 +1,12 @@
-export const GetTransactions = async (req, res) => {};
+// todo
+/** GET /user/:userId/transaction recuperare una o tutte le transazioni */
+export const GetTransactions = (req, res) => { }
+// todo
+/** POST /user/:userId/transaction creare un nuovo metodo di pagamento */
+export const PostTransaction = (req, res) => { }
+// todo
+/** PUT /user/:userId/transaction/:transactionId modificare un metodo di pagamento */
+export const PutTransaction = (req, res) => { }
+// todo
+/** DELETE /user/:userId/transaction/:transactionId eliminare un metodo di pagamento */
+export const DeleteTransaction = (req, res) => { }
--- backend/routes/transactions.router.js
+++ backend/routes/transactions.router.js
@@ -3,10 +3,17 @@ import * as TransactionsController from "../controllers/transactions.controller.
 
 const Router = express.Router();
 
-Router.get("/", TransactionsController.GetTransactions);
-Router.post("/",);
-Router.put("/",);
-Router.delete("/",);
-Router.patch("/",);
+// todo
+/** GET /user/:userId/transaction recuperare una o tutte le transazioni */
+Router.get("/user/:userId/transaction", TransactionsController.GetTransactions);
+// todo
+/** POST /user/:userId/transaction creare un nuovo metodo di pagamento */
+Router.post("/user/:userId/transaction", TransactionsController.PostTransaction);
+// todo
+/** PUT /user/:userId/transaction/:transactionId modificare un metodo di pagamento */
+Router.put("/user/:userId/transaction/:transactionId", TransactionsController.PutTransaction);
+// todo
+/** DELETE /user/:userId/transaction/:transactionId eliminare un metodo di pagamento */
+Router.delete("/user/:userId/transaction/:transactionId", TransactionsController.DeleteTransaction);
 
 export default Router;
